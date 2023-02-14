e59babf602444e3f10fe872386c26c088c42a32d
Michelle Byrnes <michelle.elizabeth.byrnes@gmail.com>
2023-02-14 20:35:07 +0000

    Add split battle icons

--- src/battle_controller_player.c
+++ src/battle_controller_player.c
@@ -91,6 +91,8 @@ static void PlayerHandleBattleAnimation(void);
 static void PlayerHandleLinkStandbyMsg(void);
 static void PlayerHandleResetActionMoveSelection(void);
 static void PlayerHandleEndLinkBattle(void);
+static void MoveSelectionDisplaySplitIcon(void);
+int GetBattleMoveSplit(u32 move);
 static void PlayerCmdEnd(void);
 
 static void PlayerBufferRunCommand(void);
@@ -1505,6 +1507,7 @@ static void MoveSelectionDisplayMoveType(void)
 
     StringCopy(txtPtr, gTypeNames[gBattleMoves[moveInfo->moves[gMoveSelectionCursor[gActiveBattler]]].type]);
     BattlePutTextOnWindow(gDisplayedStringBattle, B_WIN_MOVE_TYPE);
+    MoveSelectionDisplaySplitIcon();
 }
 
 static void MoveSelectionCreateCursorAt(u8 cursorPosition, u8 baseTileNum)
@@ -3140,6 +3143,29 @@ static void PlayerHandleEndLinkBattle(void)
     BeginFastPaletteFade(3);
     PlayerBufferExecCompleted();
     gBattlerControllerFuncs[gActiveBattler] = SetBattleEndCallbacks;
+}
+
+static void MoveSelectionDisplaySplitIcon(void){
+	static const u16 sSplitIcons_Pal[] = INCBIN_U16("graphics/interface/split_icons_battle.gbapal");
+	static const u8 sSplitIcons_Gfx[] = INCBIN_U8("graphics/interface/split_icons_battle.4bpp");
+    struct ChooseMoveStruct *moveInfo;
+	int icon;
+    
+    moveInfo = (struct ChooseMoveStruct *)(&gBattleBufferA[gActiveBattler][4]);
+	icon = GetBattleMoveSplit(moveInfo->moves[gMoveSelectionCursor[gActiveBattler]]);
+	LoadPalette(sSplitIcons_Pal, 10 * 0x10, 0x20);
+	BlitBitmapToWindow(B_WIN_SPLIT_ICON, sSplitIcons_Gfx + 0x80 * icon, 0, 0, 16, 16);
+	PutWindowTilemap(B_WIN_SPLIT_ICON);
+	CopyWindowToVram(B_WIN_SPLIT_ICON, 3);
+}
+
+int GetBattleMoveSplit(u32 move) {
+    if (IS_TYPE_PHYSICAL(gBattleMoves[move].type))
+        return 0x0;
+    else if (IS_TYPE_SPECIAL(gBattleMoves[move].type))
+        return 0x1;
+    else
+        return 0x2;
 }
 
 static void PlayerCmdEnd(void)
